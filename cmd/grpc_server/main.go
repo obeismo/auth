a790fce79eaf995d6cd123aaf8ae777420c15dcf
Maxim <rrb0md@gmail.com>
2025-04-21 08:11:04 +0700

    .

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -2,28 +2,19 @@ package main
 
 import (
 	"context"
-	"github.com/obeismo/auth/internal/repository"
-
-	desc "github.com/obeismo/auth/pkg/auth/v1"
+	"log"
 )
 
-type server struct {
-	desc.UnimplementedAuthV1Server
-	authRepository repository.AuthRepository
-}
-
-func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
-	return nil, nil
-}
-
 func main() {
-	//a, err := app.NewApp(ctx)
-	//if err != nil {
-	//	log.Fatalf("failed to init app: %s", err.Error())
-	//}
-	//
-	//err = a.Run()
-	//if err != nil {
-	//	log.Fatalf("failed to run app: %s", err.Error())
-	//}
+	ctx := context.Background()
+
+	a, err := app.NewApp(ctx)
+	if err != nil {
+		log.Fatalf("failed to init app: %s", err.Error())
+	}
+
+	err = a.Run()
+	if err != nil {
+		log.Fatalf("failed to run app: %s", err.Error())
+	}
 }
